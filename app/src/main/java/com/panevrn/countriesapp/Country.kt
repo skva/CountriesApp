42b419c09e4f1d9e89a464e93217a73b88102c8d
skva <skva1er@yandex.ru>
2022-05-13 17:10:47 +0400

    Initial commit

---
+++ app/src/main/java/com/panevrn/countriesapp/Country.kt
@@ -0,0 +1,14 @@
+package com.panevrn.countriesapp
+
+data class Country (
+    val name: String,
+    val capital: String,
+    val population: Long,
+    val area: Long,
+    val languages: List<Language>,
+    val flag: String
+)
+
+data class Language (
+    val name: String
+    )
---
+++ app/src/main/java/com/panevrn/countriesapp/RemoteDataSource.kt
@@ -0,0 +1,19 @@
+package com.panevrn.countriesapp
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+
+interface RestCountriesApi {
+    @GET("name/{name}")
+    suspend fun getCountryByName(@Path("name") cityName: String): List<Country>
+}
+
+var retrofit = Retrofit.Builder()
+    .baseUrl("https://restcountries.com/v2/")
+    .addConverterFactory(GsonConverterFactory.create())
+    .build()
+
+var restCountriesApi: RestCountriesApi = retrofit.create(RestCountriesApi::class.java)
